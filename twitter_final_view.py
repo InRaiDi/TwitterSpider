77bdddaef294b85a5e2fcece94c33888676f70b5
Dinara <inraidi@gmail.com>
2016-05-03 13:47:26 +0300

    Loop modification

--- twitter_final_view.py
+++ twitter_final_view.py
@@ -4,6 +4,6 @@ with open('twitter_results.json') as data_file:
     data = json.load(data_file)
 output = open('twitter_view.html', 'w+')
 
-for i in range (0,19):
+for i in range (20):
 	str="<b>" + data[i]['date'] + '</b>: ' + data[i]['tweet']
 	output.write(str.encode('utf-8'))
